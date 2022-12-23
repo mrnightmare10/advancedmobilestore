586752a11254753bcd98e8b00df57e44011237bd
mrnightmare10 <111358062+mrnightmare10@users.noreply.github.com>
2022-12-23 17:09:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,17 +17,11 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
-  async headers() {
+ async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/:path*'
       }
     ]
   },
