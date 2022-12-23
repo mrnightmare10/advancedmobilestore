08be5195bfe87cca6873bad0f344356a4c85f302
mrnightmare10 <111358062+mrnightmare10@users.noreply.github.com>
2022-12-23 19:14:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,12 +17,4 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
- async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://advancedmobilestore.vercel.app/:path*'
-      }
-    ]
-  },
 };
